0d58cf0d499c6c74f0e184886b342e6c75d0f3b0
kolle <kolle@jane12-3.kihdmn>
2016-02-11 15:03:32 +0300

    +add server

---
+++ life-web-app/src/main/java/telemedic/lab/ru/life/web/app/ServletDispetcher.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telemedic.lab.ru.life.web.app;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kolle
+ */
+public class ServletDispetcher extends HttpServlet {
+// Getting a String object from the applet and send it back.
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            response.setContentType("application/x-java-serialized-object");
+            InputStream inputStream = request.getInputStream();
+            ObjectInputStream inputFromApplet = new ObjectInputStream(inputStream);
+            String string = (String) inputFromApplet.readObject();
+            // getting string value and passing to applet
+            OutputStream outputStream = response.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(string);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            log(e.getLocalizedMessage());
+        }
+    }
+}
